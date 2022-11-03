13a0529ec71fc20c782d203097a3f3a8272c8748
Elsammit <hhashimo25@gmail.com>
2022-11-03 18:44:50 +0900

    お試しコミット

--- WinScreenRec/MainViewModel.cs
+++ WinScreenRec/MainViewModel.cs
@@ -6,7 +6,78 @@ using System.Threading.Tasks;
 
 namespace WinScreenRec
 {
-    class MainViewModel
+    class MainViewModel : BindingBase
     {
+
+        private String _StartBtnContent = "";
+        public String StartBtnContent {
+            get => _StartBtnContent;
+            set
+            {
+                _StartBtnContent = value;
+                OnPropertyChanged(nameof(StartBtnContent));
+            }
+        }
+
+        private DelegateCommand _StartBtnOnClick = null;
+        public DelegateCommand StartBtnOnClick 
+        {
+            get
+            {
+                if(_StartBtnOnClick == null)
+                {
+                    _StartBtnOnClick = new DelegateCommand(StartRecordFunc);
+                }
+                return _StartBtnOnClick;
+            }
+        }
+
+        private System.Windows.Media.ImageSource _ImageArea = null;
+        public System.Windows.Media.ImageSource ImageArea {
+            get => _ImageArea;
+            set
+            {
+                _ImageArea = value;
+                OnPropertyChanged(nameof(ImageArea));
+            }
+        }
+
+        private double _RecBorderOpacity = 0;
+        public double RecBorderOpacity {
+            get => _RecBorderOpacity;
+            set
+            {
+                _RecBorderOpacity = value;
+                OnPropertyChanged(nameof(RecBorderOpacity));
+            }
+        }
+
+        private String _RecTimerContent = "";
+        public String RecTimerContent {
+            get => _RecTimerContent;
+            set
+            {
+                _RecTimerContent = value;
+                OnPropertyChanged(nameof(RecTimerContent));
+            }
+        }
+
+        private double _RecTimerOpacity = 0;
+        public double RecTimerOpacity {
+            get => _RecTimerOpacity;
+            set
+            {
+                _RecTimerOpacity = value;
+                OnPropertyChanged(nameof(RecTimerOpacity));
+            }
+        }
+
+
+        private void StartRecordFunc()
+        {
+            Console.WriteLine("Start Record Func");
+        }
+
+
     }
 }
